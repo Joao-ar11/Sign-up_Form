21fbe3851c7199a623dade3a7080dcfa872af7ca
joao-ar11 <jarlpereira29@gmail.com>
2023-03-19 10:15:28 -0300

    Add validation for both passwords

--- javascript.js
+++ javascript.js
@@ -1,23 +1,37 @@
 const information = document.querySelectorAll(".information");
 information.forEach((item) => {
     item.addEventListener("input", () => {
-        if (!item.validity.valid && item.value !== "") {
+        if (!item.checkValidity() && item.value !== "") {
+            item.classList.remove("valid");
+            item.classList.add("invalid");
+        } else if (item.value === "") {
+            item.classList.remove("valid");
             item.classList.add("invalid");
         } else {
             item.classList.remove("invalid");
+            item.classList.add("valid")
         }
-    });
+    })
 });
 
 const passwords = document.querySelectorAll(".password");
-passwords.forEach((item) => {
-    item.addEventListener("input", () => {
-        if (passwords[0].value !== passwords[1].value) {
-            passwords[0].classList.add("invalid");
-            passwords[1].classList.add("invalid");
-        } else {
-            passwords[0].classList.remove("invalid");
-            passwords[1].classList.remove("invalid");
-        }
-    });
-});
+passwords.forEach((item) => item.addEventListener("input", addPasswordValidation));
+
+function addPasswordValidation() {
+    const passwords = document.querySelectorAll(".password");
+    if (passwords[0].value !== passwords[1].value) {
+        passwords[0].setCustomValidity("The passwords doesn't match");
+        passwords[1].setCustomValidity("The passwords doesn't match");
+        passwords[0].classList.remove("valid");
+        passwords[1].classList.remove("valid");
+        passwords[0].classList.add("invalid");
+        passwords[1].classList.add("invalid");
+    } else {
+        passwords[0].setCustomValidity("");
+        passwords[1].setCustomValidity("");
+        passwords[0].classList.remove("invalid");
+        passwords[1].classList.remove("invalid");
+        passwords[0].classList.add("valid");
+        passwords[1].classList.add("valid");
+    }
+}
